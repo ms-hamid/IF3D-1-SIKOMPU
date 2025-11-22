243567512f893d80590ea8ab1befd644c53e6296
elisabeth1047 <elisabethmargareta02@gmail.com>
2025-11-22 13:54:13 +0700

    commit

---
+++ SIKOMPU/database/migrations/2025_11_20_171625_update_jabatan_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->enum('jabatan', [
+                'Kepala Jurusan',
+                'Sekretaris Jurusan',
+                'Kepala Program Studi',
+                'Dosen',
+                'Laboran'
+            ])->change();
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
